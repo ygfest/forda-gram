fbe4e732bbe815c993d8495b5ed1b88530630089
ygfest <stefanosanesteban1018@gmail.com>
2025-01-29 22:43:24 +0800

    Add the component fallback for sidebar section if the user is unauthenticated

--- src/components/side-bar-section.tsx
+++ src/components/side-bar-section.tsx
@@ -9,10 +9,12 @@ import {
 } from "./ui/card";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { getUserDataFromDb } from "../../actions/user.action";
+import { Button } from "./ui/button";
+import { SignIn, SignInButton, SignUp, SignUpButton } from "@clerk/nextjs";
 
 async function SideBarSection() {
   const authUser = await currentUser();
-  if (!authUser) return;
+  if (!authUser) return <UnAuthenticatedSideBar />;
 
   const user = await getUserDataFromDb(authUser.id);
   if (!user) return null;
@@ -20,7 +22,7 @@ async function SideBarSection() {
     <div>
       <Card>
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
+          <CardTitle>{user.name}</CardTitle>
           <CardDescription>Card Description</CardDescription>
         </CardHeader>
         <CardContent>
@@ -35,3 +37,31 @@ async function SideBarSection() {
 }
 
 export default SideBarSection;
+
+const UnAuthenticatedSideBar = () => {
+  return (
+    <div className="sticky top-20">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center text-xl font-sem-bold">
+            Welcome Back!
+          </CardTitle>
+          <CardDescription className="text-center text-muted-foreground mb-4">
+            Login to access your profile and connect with others
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton mode="modal">
+            <Button className="w-full" variant="outline">
+              {" "}
+              Login{" "}
+            </Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button className="w-full mt-2">Sign Up</Button>
+          </SignUpButton>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
