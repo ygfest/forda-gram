25f8627e5fb2db3784e855ae6fe474ac25b271be
ygfest <stefanosanesteban1018@gmail.com>
2025-01-31 20:44:24 +0800

    Fix the issue for following the user

--- src/components/follow-button.tsx
+++ src/components/follow-button.tsx
@@ -6,7 +6,7 @@ import { auth } from "@clerk/nextjs/server";
 import { toggleFollow } from "../../actions/user.action";
 import toast from "react-hot-toast";
 
-function FollowButton(userId: { userId: string }) {
+function FollowButton({ userId }: { userId: string }) {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleFollow = async () => {
